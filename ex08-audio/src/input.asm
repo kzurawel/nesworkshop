b0ea026e4b1807580e23934a6b61362c0aa528ec
Kevin Zurawel <kzurawel@gmail.com>
2018-01-05 13:22:59 -0600

    CONT_PORT_ -> CONTROLLER

--- ex08-audio/src/input.asm
+++ ex08-audio/src/input.asm
@@ -11,18 +11,18 @@
   PHA
   PHP
 
-  ; write a 1, then a 0, to CONT_PORT_1
+  ; write a 1, then a 0, to CONTROLLER1
   ; to latch button states
   LDA #$01
-  STA CONT_PORT_1
+  STA CONTROLLER1
   LDA #$00
-  STA CONT_PORT_1
+  STA CONTROLLER1
 
   LDA #$01
   STA controller1 ; move the '1' until done
                   ; (ring counter)
 get_buttons:
-  LDA CONT_PORT_1   ; read from controller port
+  LDA CONTROLLER1   ; read from controller port
   LSR A             ; shift accumulator bit 0 into carry flag
   ROL controller1   ; move carry flag into controller1
   BCC get_buttons   ; repeat until original '1' is moved to carry
